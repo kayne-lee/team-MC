66b87bee5705ad288ab0aefcf7b9e0bf638158d5
kayne-lee <kayne.lee2@outlook.com>
2024-11-10 10:38:02 -0500

    Start of landing page

---
+++ fe/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust according to where your files are located
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

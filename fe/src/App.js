285997eb71e48e202828b81253e95cfcae3566bd
kayne-lee <kayne.lee2@outlook.com>
2024-11-10 17:12:14 -0500

    Complete frontend of the email signup webiste

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,11 +1,18 @@
-import Login from './components/Login';
+import Signup from './components/Signup';
 import Landing from './components/Landing';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Landing />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

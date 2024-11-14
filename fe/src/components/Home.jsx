7abd9cbf1b18ebacd01085f63d7341539b2bda3f
ChloeHouvardas <chloehouvardas404@gmail.com>
2024-11-14 10:44:14 -0500

    Fixed spacing for day boxes and added date selector

--- fe/src/components/Home.jsx
+++ fe/src/components/Home.jsx
@@ -3,14 +3,20 @@ import '../styles/home.css'
 
 export default function Home() {
     return (
-        <div className="flex flex-row bg-white justify-between w-full">
-            <div className="bg-gray-100 flex-1 text-center day-box">MONDAY</div>
-            <div className="bg-gray-100 flex-1 text-center day-box">TUESDAY</div>
-            <div className="bg-gray-100 flex-1 text-center day-box">WEDNESDAY</div>
-            <div className="bg-gray-100 flex-1 text-center day-box">THURSDAY</div>
-            <div className="bg-gray-100 flex-1 text-center day-box">FRIDAY</div>
-            <div className="bg-gray-100 flex-1 text-center day-box">SATURDAY</div>
-            <div className="bg-gray-100 flex-1 text-center day-box">SUNDAY</div>
+        <div className="flex flex-col background min-h-screen">
+            <div className="nav-placeholder h-36"></div>
+            <div className="flex flex-row justify-between w-full flex-grow mb-16">
+                <div className="day-box">MONDAY</div>
+                <div className="day-box">TUESDAY</div>
+                <div className="day-box">WEDNESDAY</div>
+                <div className="day-box">THURSDAY</div>
+                <div className="day-box">FRIDAY</div>
+                <div className="day-box">SATURDAY</div>
+                <div className="day-box">SUNDAY</div>
+            </div>
+            <div className="date-selector bg-gray-100 w-full h-52 flex items-center justify-center fixed bottom-0">
+                Hello World
+            </div>
         </div>
     );
 }

2095ed4b943f1ff0e85e359daab896d64d311533
ChloeHouvardas <chloehouvardas404@gmail.com>
2024-11-14 10:59:56 -0500

    Added buttons to date selector

--- fe/src/components/Home.jsx
+++ fe/src/components/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import '../styles/home.css'
-
 export default function Home() {
     return (
         <div className="flex flex-col background min-h-screen">
             <div className="nav-placeholder h-36"></div>
-            <div className="flex flex-row justify-between w-full flex-grow mb-16">
+            <div className="flex flex-row justify-between w-full flex-grow mb-16 pb-52">
                 <div className="day-box">MONDAY</div>
                 <div className="day-box">TUESDAY</div>
                 <div className="day-box">WEDNESDAY</div>
@@ -14,8 +13,13 @@ export default function Home() {
                 <div className="day-box">SATURDAY</div>
                 <div className="day-box">SUNDAY</div>
             </div>
-            <div className="date-selector bg-gray-100 w-full h-52 flex items-center justify-center fixed bottom-0">
-                Hello World
+            <div className="date-selector w-full h-52 flex items-center justify-center fixed bottom-0 mx-auto">
+                <div className="select-buttons flex flex-row justify-between w-full">
+                    <button><div className="cursor-pointer h-[81px] w-[81px] text-[24px] text-white rounded-[45px] bg-[#8338EC] mt-[38px] flex justify-center items-center">1</div></button>
+                    <button><div className="cursor-pointer h-[81px] w-[81px] text-[24px] text-white rounded-[45px] bg-[#8338EC] mt-[38px] flex justify-center items-center">2</div></button>
+                    <button><div className="cursor-pointer h-[81px] w-[81px] text-[24px] text-white rounded-[45px] bg-[#8338EC] mt-[38px] flex justify-center items-center">3</div></button>
+                
+                </div>
             </div>
         </div>
     );

209db2dfd9e4e318791e5dbcb4aa9496b60200c8
kayne-lee <kayne.lee2@outlook.com>
2024-11-14 17:47:06 -0500

    add local host

--- email/index.js
+++ email/index.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
-const allowedOrigins = ['https://team-mc.vercel.app/', 'https://team-mc.vercel.app/signup'];
+const allowedOrigins = ['https://team-mc.vercel.app', 'https://team-mc.vercel.app/signup', 'http://localhost:3000/'];
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {

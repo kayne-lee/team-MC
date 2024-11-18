0965c72b18be68660ea9afee4f20b802d53914b1
ChloeHouvardas <chloehouvardas404@gmail.com>
2024-11-17 19:00:44 -0500

    Reconfigured layout of elements

--- fe/src/components/Home.jsx
+++ fe/src/components/Home.jsx
@@ -4,16 +4,18 @@ export default function Home() {
     return (
         <div className="flex flex-col background min-h-screen">
             <div className="nav-placeholder h-36"></div>
-            <div className="flex flex-row justify-between w-full flex-grow mb-16 pb-52">
+            <div className="flex flex-row justify-between w-full flex-grow h-[300px] mb-8 pb-2">
                 <div className="day-box">MONDAY</div>
                 <div className="day-box">TUESDAY</div>
                 <div className="day-box">WEDNESDAY</div>
                 <div className="day-box">THURSDAY</div>
                 <div className="day-box">FRIDAY</div>
+            </div>
+            <div className='flex flex-row justify-between w-full flex-grow mb-16 pb-32'>
                 <div className="day-box">SATURDAY</div>
                 <div className="day-box">SUNDAY</div>
             </div>
-            <div className="date-selector w-full h-52 flex items-center justify-center fixed bottom-0 mx-auto">
+            <div className="date-selector w-full h-[145px] flex items-center justify-center fixed bottom-0 mx-auto">
                 <div className="select-buttons flex flex-row justify-between w-full">
                     <button><div className="cursor-pointer h-[81px] w-[81px] text-[24px] text-white rounded-[45px] bg-[#8338EC] mt-[38px] flex justify-center items-center">1</div></button>
                     <button><div className="cursor-pointer h-[81px] w-[81px] text-[24px] text-white rounded-[45px] bg-[#8338EC] mt-[38px] flex justify-center items-center">2</div></button>

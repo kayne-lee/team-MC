be91440c795438c81ff8bead59f017c82f235804
kayne-lee <kayne.lee2@outlook.com>
2024-11-19 22:28:13 -0500

    Divide into pages

--- syllabus/src/App.js
+++ syllabus/src/App.js
@@ -1,63 +1,15 @@
-import React, { useState } from 'react';
-import Navbar from './navbar/Navbar';
-import './App.css';
-import fileIcon from './assets/file_icon.png';
-import trashIcon from './assets/trash.png';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import SylaScan from './components/SylaScan';
 
-function SyllabusPuller() {
-  const [uploadedSyllabi, setUploadedSyllabi] = useState([]);
-
-  const handleFileChange = (e) => {
-    const selectedFile = e.target.files[0];
-    if (selectedFile) {
-      setUploadedSyllabi([...uploadedSyllabi, selectedFile.name]);
-    }
-  };
-
-  const handleDelete = (index) => {
-    const updatedList = uploadedSyllabi.filter((_, i) => i !== index);
-    setUploadedSyllabi(updatedList);
-  };
-
+export default function App() {
   return (
-    <div className="syllabus-puller">
-      <Navbar />
-      <div className="upload-section">
-        <h2>Upload Syllabus</h2>
-        <div className="upload-box">
-          <div className="icon">
-            <img src={fileIcon} alt="File Icon" className="file-icon" />
-          </div>
-          <button
-            className="upload-button"
-            onClick={() => document.getElementById('fileInput').click()}
-          >
-            Upload or Attach Syllabus
-          </button>
-          <input
-            type="file"
-            id="fileInput"
-            accept=".pdf"
-            style={{ display: 'none' }}
-            onChange={handleFileChange}
-          />
-        </div>
-      </div>
-
-      <h2>Uploaded Syllabi</h2>
-      <div className="uploaded-syllabi">
-        <ul>
-          {uploadedSyllabi.map((syllabus, index) => (
-            <li key={index} className="syllabus-item">
-              <span>{syllabus}</span>
-              <img src={trashIcon} alt="Trash Icon" className="delete-button" onClick={() => handleDelete(index)}>
-              </img>
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SylaScan />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
-
-export default SyllabusPuller;

c78029e1fa9ac32cfe7a1b357b021bf5f40fa557
kayne-lee <kayne.lee2@outlook.com>
2024-11-20 20:58:28 -0500

    complete login, prints jwt

--- syllabus/src/components/Login.js
+++ syllabus/src/components/Login.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../styles/login.css'
 
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault(); 
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+        "email": email,
+        "password": password
+        });
+
+        const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+        };
+
+        fetch("http://localhost:8080/auth/login", requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error));
+      };
+    
   return (
-    <div className="h-[100vh]">
-        Login
+    <div className="h-screen relative">
+      {/* Top Section */}
+      <div className="absolute top-[43px] left-[63px] w-full">
+        <img src="/nucleus.png" alt="" className="w-[141px] "/>
+      </div>
+
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col  w-[791px] welcome justify-evenly items-center">
+        <div className="text-[#F5F5F5] text-center font-poppins text-[50px] font-bold pt-[57px]">
+            Welcome Back
         </div>
+        <div className="w-[567px] h-[77px] rounded-[20px] bg-[#F3F3F3] mt-[29px]">
+            <div className="mt-[11px] ml-[23px] text-[#BFA1E9] font-[700] font-poppins text-[16px]">
+                Email
+            </div>
+            <input
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full h-[40px] rounded-[25px] pb-[5px] px-[23px] bg-[#F3F3F3] text-[#333] focus:outline-none"
+            />
+        </div>
+
+        <div className="w-[567px] h-[77px] rounded-[20px] bg-[#F3F3F3] mt-[10px]">
+            <div className="mt-[11px] ml-[23px] text-[#BFA1E9] font-[700] font-poppins text-[16px]">
+                Password
+            </div>
+            <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full h-[40px] rounded-[25px] pb-[5px] px-[23px] bg-[#F3F3F3] text-[#333] focus:outline-none"
+            />
+        </div>
+
+        <div onClick={handleLogin} className="w-[300px] h-[57px] bg-[#F3F3F3] flex flex-row justify-center items-center rounded-[25px] mt-[29px] mb-[29px] hover:bg-[#DECBF8] cursor-pointer">
+            <div className="text-[#8338EC] text-center font-poppins text-[30px] text-center font-bold">
+                Login
+            </div>
+        </div>
+
+
+      </div>
+    </div>
   )
 }

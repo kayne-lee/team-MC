f049e93d6f178349502ceebfe3e1854ca4d21432
kayne-lee <kayne.lee2@outlook.com>
2024-11-20 19:53:07 -0500

    add tailwind and start login

--- syllabus/src/App.js
+++ syllabus/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import SylaScan from './components/SylaScan';
+import Login from './components/Login';
 
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <Router>
         <Routes>
           <Route path="/" element={<SylaScan />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
---
+++ syllabus/src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className="h-[100vh]">
+        Login
+        </div>
+  )
+}

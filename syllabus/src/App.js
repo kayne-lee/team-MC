f278a55c7c570b19a6881ccb56a49b8645f853dd
ChloeHouvardas <chloehouvardas404@gmail.com>
2024-11-21 13:00:45 -0500

    Moved task page files to correct folder

--- syllabus/src/App.js
+++ syllabus/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import SylaScan from './components/SylaScan';
+import TasksPage from './components/TasksPage';
 
 export default function App() {
   return (
@@ -8,6 +9,8 @@ export default function App() {
       <Router>
         <Routes>
           <Route path="/" element={<SylaScan />} />
+          <Route path="/tasksPage" element={<SylaScan />} />
+          <Route path="/tasksPage" element={<TasksPage />} />
         </Routes>
       </Router>
     </div>

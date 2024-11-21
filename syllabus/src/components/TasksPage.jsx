1e2bcbf1810342622866ccc06c381486265b4d0e
ChloeHouvardas <chloehouvardas404@gmail.com>
2024-11-21 13:47:55 -0500

    Fixed font styling

--- syllabus/src/components/TasksPage.jsx
+++ syllabus/src/components/TasksPage.jsx
@@ -1,27 +1,30 @@
 import '../styles/tasksPage.css';
+import Navbar from '../navbar/Navbar';
 
 const TasksPage = () => {
     return (
-        <div className="parent min-h-screen">
+        <div className=" tasks-page parent min-h-screen">
+            <Navbar />
             <h1 className="header">Upcoming Tasks</h1>
             <div className="layout-container ">
                 {/* Left Large Box */}
+                {/* CURRENT DAY BOX */}
                 <div className="left-box">
-                    <h2>Tuesday, November 9th</h2>
+                    <h2 className="main-date-header">Tuesday, November 9th</h2>
                     <div className="task-list">
-                        <h3>QTMA Work</h3>
-                        <ul>
+                        <h3 className="main-task-header">QTMA Work</h3>
+                        <ul className="main-list">
                             <li>Create Slideshow</li>
                             <li>Design Logos</li>
                             <li>Customer Research</li>
                         </ul>
-                        <h3>MATH 121: Calculus</h3>
+                        <h3 className="main-task-header">MATH 121: Calculus</h3>
                         <ul>
                             <li>Week 8 Videos</li>
                             <li>Week 8 WebWork</li>
                             <li>Start Week 8 Tutorial</li>
                         </ul>
-                        <h3>CISC 101: Computer Science</h3>
+                        <h3 className="main-task-header">CISC 101: Computer Science</h3>
                         <ul>
                             <li>Runestone Week 8: Files</li>
                         </ul>
@@ -29,20 +32,21 @@ const TasksPage = () => {
                 </div>
 
                 {/* Right Grid of Boxes */}
+                {/* UPCOMING DAY BOXES*/}
                 <div className="right-grid">
                     <div className="grid-box">
-                        <h3>Wednesday, November 10th</h3>
-                        <p>English Paper</p>
-                        <ul>
+                        <h3 className="date-header">Wednesday, November 10th</h3>
+                        <h3 className="task-header">English Paper</h3>
+                        <ul className="sub-list">
                             <li>Look Over Rubric</li>
                             <li>Brainstorm Ideas</li>
                             <li>Customer Research</li>
                         </ul>
                     </div>
                     <div className="grid-box">
-                        <h3>Friday, November 12th</h3>
-                        <p>Finish English Paper</p>
-                        <ul>
+                        <h3 className="date-header">Friday, November 12th</h3>
+                        <h3 className="task-header">Finish English Paper</h3>
+                        <ul className="sub-list">
                             <li>Zoom Meeting with Team</li>
                             <li>Help Ryan with slideshow ideas</li>
                             <li>Work Cited</li>
@@ -52,9 +56,9 @@ const TasksPage = () => {
                         <span>+</span>
                     </div>
                     <div className="grid-box">
-                        <h3>Saturday, November 13th</h3>
-                        <p>QTMA</p>
-                        <ul>
+                        <h3 className="date-header"> Saturday, November 13th</h3>
+                        <h3 className="task-header">QTMA</h3>
+                        <ul className="sub-list">
                             <li>Meeting at 4:00 PM</li>
                         </ul>
                     </div>

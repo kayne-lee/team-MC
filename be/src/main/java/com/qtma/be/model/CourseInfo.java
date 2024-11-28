4cd1f6aa2706f97a35f682911da64aece3275282
MatthewKompel <matthewkompel@gmail.com>
2024-11-27 19:50:11 -0500

    added new prompt info

---
+++ be/src/main/java/com/qtma/be/model/CourseInfo.java
@@ -0,0 +1,17 @@
+package com.qtma.be.model;
+
+import lombok.*;
+
+@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseInfo {
+    private String instructorName;
+    private String instructorEmail;
+    private String officeHoursTime;
+    private String officeHoursLocation;
+    private String category;
+
+}

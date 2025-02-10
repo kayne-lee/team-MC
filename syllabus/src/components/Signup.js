3eff54c3615826086bc75395030b3ad656703603
kayne-lee <kayne.lee2@outlook.com>
2025-02-10 10:08:52 -0500

    [add] phone number to signup

--- syllabus/src/components/Signup.js
+++ syllabus/src/components/Signup.js
@@ -6,6 +6,7 @@ import Error from '../assets/error.png'
 export default function Signup() {
     const [name, setName] = useState('');
     const [lastName, setLastName] = useState('');
+    const [phone, setPhone] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPass, setConfirmPass] = useState('');
@@ -18,7 +19,9 @@ export default function Signup() {
         e.preventDefault(); 
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
-        if (password != confirmPass) {
+        if (!name || !lastName || !email || !password || !confirmPass) {
+            setMessage("All fields are required")
+        }else if (password != confirmPass) {
             setMessage("Ensure passwords match")
         } else {
             const raw = JSON.stringify({
@@ -27,7 +30,9 @@ export default function Signup() {
                 "email": email,
                 "password": password,
                 "friends": [],
-                "streak": 0
+                "streak": 0,
+                "phone": phone,
+                "notification": 3
                 });
         
                 const requestOptions = {
@@ -132,6 +137,16 @@ export default function Signup() {
                 <input
                 type="email"
                 onChange={(e) => setEmail(e.target.value)}
+                className="w-full h-[40px] rounded-[25px] pb-[5px] px-[23px] bg-[#F3F3F3] text-[#333] focus:outline-none"
+                />
+            </div>
+            <div className="w-[528px] h-[77px] rounded-[20px] bg-[#F3F3F3] mt-[20px]">
+                <div className="mt-[11px] ml-[23px] text-[#BFA1E9] font-[700] font-poppins text-[16px]">
+                Phone Number
+                </div>
+                <input
+                type="phone"
+                onChange={(e) => setPhone(e.target.value)}
                 className="w-full h-[40px] rounded-[25px] pb-[5px] px-[23px] bg-[#F3F3F3] text-[#333] focus:outline-none"
                 />
             </div>

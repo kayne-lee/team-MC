107ca9ef59fbbdbef801998f08078c7714c6fa94
kayne-lee <kayne.lee2@outlook.com>
2025-03-13 20:12:28 -0400

    minor bug

--- be/src/main/java/com/qtma/be/controller/DataController.java
+++ be/src/main/java/com/qtma/be/controller/DataController.java
@@ -209,10 +209,10 @@ public class DataController {
                         courseMap.put("officeLocation", course.getCourseInfo().getOfficeHoursLocation());
                         courseMap.put("officeHours", course.getCourseInfo().getOfficeHoursTime());
                         courseMap.put("category", course.getCourseInfo().getCategory());
-                        courseMap.put("requiredResouces", course.getCourseInfo().getCategory());
-                        courseMap.put("attendanceAndLate", course.getCourseInfo().getCategory());
-                        courseMap.put("gradingPolicy", course.getCourseInfo().getCategory());
-                        courseMap.put("teachingAssistantInfo", course.getCourseInfo().getCategory());
+                        courseMap.put("requiredResouces", course.getCourseInfo().getRequiredResouces());
+                        courseMap.put("attendanceAndLate", course.getCourseInfo().getAttendanceAndLate());
+                        courseMap.put("gradingPolicy", course.getCourseInfo().getGradingPolicy());
+                        courseMap.put("teachingAssistantInfo", course.getCourseInfo().getTeachingAssistantInfo());
                         // Assignments with grades and completion status
                         List<Map<String, Object>> assignments = course.getAssignments().stream().map(assignment -> {
                             Map<String, Object> assignmentMap = new HashMap<>();
